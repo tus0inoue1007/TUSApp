a1c63f1c53761d8e64a5878ca50bb02741eab5fa
tus0inoue1007 <4620007@ed.tus.ac.jp>
2023-07-13 15:16:59 +0900

    pageの中身を綺麗に書き換えた

--- tokyo-app/src/app/mapbox/page.tsx
+++ tokyo-app/src/app/mapbox/page.tsx
@@ -1,247 +1,191 @@
 'use client'
 
 import * as React from 'react';
-import {render} from 'react-dom';
-import Card from '@/components/card';
 import {useState, useMemo, useCallback,useRef,useEffect} from 'react';
 import Map, { Marker,Popup,  MapRef,Source, Layer } from "react-map-gl";
-import type {MapboxStyle, MapLayerMouseEvent} from 'react-map-gl';
+import type {MapboxStyle, MapEvent, MapLayerMouseEvent, MarkerEvent} from 'react-map-gl';
 import type {MarkerDragEvent, LngLat} from 'react-map-gl';
 import bbox from '@turf/bbox';
 import {dataLayer,lineLayer} from './map-style';
-// import Pin from './pin';
 import Pin from '@/components/pin';
 import RecommendPin from '@/components/recommendpin';
 import RecommendSheet from '@/components/recommendsheet';
-import SimpleBox from '@/components/simplebox';
-import Sheet from '@/components/sheet';
-import axios from 'axios'
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./style.css"
 
 interface Mark {
-  lng:number;
-  lat:number;
+    prefecture: string;
+    lng:number;
+    lat:number;  
+    city: string;
+    image: string;
+    feature: string;
 }
-interface SheetInfo  {
-  prefecture: string;
-  image: string;
-  city: string;
-  feature: string;
-}
+
 
 function App() {
 
-  const [allData, setAllData] = useState(null);
-  const [recommendData,setRecommendData] = useState(null);
-  const mapRef = useRef<MapRef>();
-  const [marker, setMarker] = useState<Mark | null>(null);
-  const [recommendMarker,setRecommendMarker] = useState<Mark[]>([]);
-  const [recommendInfo,setRecommendInfo] = useState<SheetInfo>({
-    prefecture: "",
-    image: "",
-    city: "",
-    feature: ""
-  });
-  const [isclicked,setIsclicked] = useState(false);
-  const [isclickedrecommend,setIsclickedrecommend] = useState(false);
-  const [clickedprefecture,setClickedprefecture] =useState<number>(0);
-  const [enableHover, setEnableHover] = useState(true);
+    const [allData, setAllData] = useState(null); //日本地図のポリゴンデータ(japan.json)
+    const [recommendData,setRecommendData] = useState(null); //おすすめ地点のデータ(japan_recommend.json)
+    const mapRef = useRef<MapRef>(); //Mapを表示
+
+    const [recommendMarker,setRecommendMarker] = useState<Mark[]>([]); //おすすめ地点にMarkerを表示
+    const [recommendsheet,setRecommendSheet] = useState<Mark>(); //おすすめ地点の情報をsheetに渡す
+
+    const [isclicked,setIsclicked] = useState(false); //Mapのクリック位置で県・県外を判定
+    const [isclickedrecommend,setIsclickedrecommend] = useState(false); //おすすめ地点のMarkerをクリックしたか判定
+    const [enableHover, setEnableHover] = useState(true); //ホバーした県の色を変更するかどうか
+    
+
+    // #################################### データの読み込み ############################
+    useEffect(() => {
+        fetch('/japan.json')
+        .then(response => response.json())
+        .then(data => setAllData(data))
+        .catch(error => console.error(error));
+    }, []);
+    useEffect(() => {
+        fetch('/japan_recommend.json')
+        .then(response => response.json())
+        .then(data =>
+            {                      
+            setRecommendData(data)
+            }
+            )
+        .catch(error => console.error(error));
+    }, []);
+    // ################################################################
 
 
+    //都道府県の色を変更する
+    const Changecolor = (prefecture) => {   
+        const updatedFeatures = allData.features.map(feature => {
+            //県の場合、一致する箇所の色を変える
+            if(prefecture){
+                if (feature.properties.pref === prefecture) {         
+                const updatedProperties = {
+                    ...feature.properties,
+                    percentile: 2,
+                };
+                return {
+                    ...feature,
+                    properties: updatedProperties,
+                };
+                } else if (feature.properties.percentile) {
+                const updatedProperties = {
+                    ...feature.properties,
+                    percentile: 1,
+                };
+                return {
+                    ...feature,
+                    properties: updatedProperties,
+                };
+                }
+                return feature;
+            }else{ //存在しない場合は全てを1にする
+                const updatedProperties = {
+                ...feature.properties,
+                percentile: 1,
+                };
+                return {
+                ...feature,
+                properties: updatedProperties,
+                };
+            }
+        });//各都道府県の色を変更する
 
-  useEffect(() => {
-    fetch('/japan.json')
-      .then(response => response.json())
-      .then(data => setAllData(data))
-      .catch(error => console.error(error));
-  }, []);
-
-
-  useEffect(() => {
-    fetch('/japan_recommend.json')
-      .then(response => response.json())
-      .then(data =>
-        {
-          // setRecommendMarker([...recommendMarker,{lng:data.features[0].geometry.coordinates[0][0],lat:data.features[0].geometry.coordinates[0][1]}])   
-          // setRecommendInfo(
-          //   {
-          //     prefecture: data.features[0].properties.name,
-          //     image: data.features[0].properties.positions[0].image,
-          //     city: data.features[0].properties.positions[0].city,
-          //     feature:  data.features[0].properties.positions[0].feature,
-          //   }
-          // )                          
-          setRecommendData(data)
-        }
-        )
-      .catch(error => console.error(error));
-  }, []);
-
-
-  const onMarkerDragStart = useCallback((event: MarkerDragEvent) => {
-    // logEvents(_events => ({..._events, onDragStart: event.lngLat}));
-  }, []);
-  const onMarkerDrag = useCallback((event: MarkerDragEvent) => {
-    // logEvents(_events => ({..._events, onDrag: event.lngLat}));
-    setMarker({
-      lng: event.lngLat.lng,
-      lat: event.lngLat.lat
-    });
-  }, []);
-  const onMarkerDragEnd = useCallback((event: MarkerDragEvent) => {
-    // logEvents(_events => ({..._events, onDragEnd: event.lngLat}));
-  }, []);
-
-  const MarkeronClick = () => {
-    setIsclickedrecommend(true)
-    console.log("markeronclick",recommendInfo)
-    // setRecommendMarker([...recommendMarker,{lng:recommendData.features[0].geometry.coordinates[0][0],lat:recommendData.features[0].geometry.coordinates[0][1]}])   
-    // setRecommendInfo(
-    //   {
-    //     prefecture: recommendData.features[0].properties.name,
-    //     image: recommendData.features[0].properties.positions[0].image,
-    //     city: recommendData.features[0].properties.positions[0].city,
-    //     feature:  recommendData.features[0].properties.positions[0].feature,
-    //   }
-    // )                 
-    // console.log(recommendInfo)
-    // console.log(recommendMarker)
-    // const res = axios.get(`/api/map/${clickedprefecture}`)
-    // console.log(res)
-  }
-
-
-  //都道府県の色を変更する
-  const Changecolor = (prefecture) => {   
-      const updatedFeatures = allData.features.map(feature => {
-        //県の場合、一致する箇所の色を変える
-        if(prefecture){
-          if (feature.properties.pref === prefecture) {         
-            const updatedProperties = {
-              ...feature.properties,
-              percentile: 2,
-            };
-            return {
-              ...feature,
-              properties: updatedProperties,
-            };
-          } else if (feature.properties.percentile) {
-            const updatedProperties = {
-              ...feature.properties,
-              percentile: 1,
-            };
-            return {
-              ...feature,
-              properties: updatedProperties,
-            };
-          }
-          return feature;
-        }else{ //存在しない場合は全てを1にする
-          const updatedProperties = {
-            ...feature.properties,
-            percentile: 1,
-          };
-          return {
-            ...feature,
-            properties: updatedProperties,
-          };
-        }
-      });
-      const updatedData = {
+        const updatedData = {
         ...allData,
         features: updatedFeatures,
-      };
-      setAllData(updatedData);
+        };
+
+        setAllData(updatedData);
     } 
 
-  //マウス操作時に実行
-  const onHover = useCallback(event => {
-    if(enableHover){
-      const { features, point: { x, y } } = event;
-      //都道府県をクリックした場合
-      if (features && features.length > 0) {
-        const prefecture =features[0].properties.pref;
-        Changecolor(prefecture)
-      } else {
-        Changecolor(false)
-      }
-    }
-  }, [allData,enableHover]);
-
-  const onClick = useCallback((event: MapLayerMouseEvent) => {  
-
-    const feat = event.features[0];       
-    if (feat) {
-      const prefecture =feat.properties?.pref;
-      setClickedprefecture(prefecture)
-      Changecolor(prefecture)
-      setEnableHover(false) 
-      setIsclicked(true)
-
-      setRecommendMarker([...recommendMarker,{lng:recommendData.features[0].geometry.coordinates[0][0],lat:recommendData.features[0].geometry.coordinates[0][1]}])   
-      setRecommendInfo(
-        {
-          prefecture: recommendData.features[prefecture-1].properties.name,
-          image: recommendData.features[prefecture-1].properties.positions[0].image,
-          city: recommendData.features[prefecture-1].properties.positions[0].city,
-          feature:  recommendData.features[prefecture-1].properties.positions[0].feature,
+    //マウスをホバーした際に実行 (県上なら色を変える、県外なら何もしない)
+    const onHover = useCallback(event => {
+        if(enableHover){
+            const { features, point: { x, y } } = event;
+            //マウスが県上なら
+            if (features && features.length > 0) {
+                const prefecture =features[0].properties.pref;
+                Changecolor(prefecture)
+            } else { //県外なら
+                Changecolor(false)
+            }
         }
-      )
+    }, [allData,enableHover]);
 
-      console.log("recommendData",recommendData)
-      console.log("recommendInfo",recommendInfo)
-      console.log("recommendMarker",recommendMarker)
+    //マウスをクリックした際に実行 (県上なら色を変えて拡大、県外なら初期画面に戻す)
+    const onClick = useCallback((event: MapLayerMouseEvent) => {  
+        const feat = event.features[0];    
+        //県上をクリックした場合   
+        if (feat) 
+        {
+            const prefecture =feat.properties?.pref; //県を表す数字
+            Changecolor(prefecture) //色を変える
+            setEnableHover(false) //ホバーをなくす
+            setIsclicked(true)  //クリックした県のおすすめ地点Markerを表示する
 
-
-
-
-
-      //zoomを操作
-      const [minLng, minLat, maxLng, maxLat] = bbox(feat);
-
-      const avgLng = (minLng + maxLng) / 2;
-      const avgLat = (minLat + maxLat) / 2;
-      setMarker({ lng: avgLng, lat: avgLat });
-
-      mapRef.current?.fitBounds(
-        [
-          [minLng, minLat],
-          [maxLng, maxLat]
-        ],
-        {padding: 40, duration: 1000}
-      );
-    }else {
-      const [minLng, minLat, maxLng, maxLat] = bbox(allData);
-      setEnableHover(true)
-      setIsclicked(false)
-      setMarker(null)
-      setIsclickedrecommend(false)
-      const japanBounds = [
-        [122, 20],
-        [154, 45] 
-      ];
-      mapRef.current.fitBounds(
-        [
-          [minLng, minLat],
-          [maxLng, maxLat]
-        ],
-        { padding: 40, duration: 1000, maxZoom: 5, bounds: japanBounds }
-      );
-    }
-  },[allData]);
-
+            recommendData.features[prefecture-1].properties.positions.map((data,index)=>(
+                setRecommendMarker(prevMarkers => [
+                    ...prevMarkers,
+                    {
+                      prefecture: feat.properties?.name,
+                      lng: recommendData.features[prefecture-1].geometry.coordinates[index][0],
+                      lat: recommendData.features[prefecture-1].geometry.coordinates[index][1],
+                      city: data.city,
+                      image: data.image,
+                      feature: data.feature
+                    }
+                ])
+            ));
+            //zoomを操作 クリックした県を拡大表示する
+            const [minLng, minLat, maxLng, maxLat] = bbox(feat);           
+            mapRef.current?.fitBounds(
+            [
+                [minLng, minLat],
+                [maxLng, maxLat]
+            ],
+            {padding: 40, duration: 1000}
+            );
+        }
+        else //県外をクリックした場合は初期状態に戻す
+        {
+            const [minLng, minLat, maxLng, maxLat] = bbox(allData);
+            setEnableHover(true) //ホバーを戻す
+            setIsclicked(false) //おすすめ地点のMarkerを消す
+            setIsclickedrecommend(false) //おすすめ情報のsheetをなくす
+            const japanBounds = [
+            [122, 20],
+            [154, 45] 
+            ];
+            mapRef.current.fitBounds(
+            [
+                [minLng, minLat],
+                [maxLng, maxLat]
+            ],
+            { padding: 40, duration: 1000, maxZoom: 5, bounds: japanBounds }
+            );
+        }
+    },[allData]);
  
-  return (
+    //マーカーをクリックした際に実行 recommend情報をsheetに表示する
+    const MarkeronClick =  useCallback((marker:Mark) => {
+        setRecommendSheet(marker)
+        setIsclickedrecommend(true)
+    },[])
+
+    return (
     <div className='content'>
-      <Map
+        <Map
         className = "map"
         ref={mapRef}
         initialViewState={{
-          longitude: 139,
-          latitude: 37.5,
-          zoom: 4.5,
+            longitude: 139,
+            latitude: 37.5,
+            zoom: 4.5,
         }}
         mapStyle="mapbox://styles/mapbox/light-v11"
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESSS_TOKEN}
@@ -250,51 +194,34 @@ function App() {
         onMouseMove={onHover}
         onClick={onClick}              
         >
-          
-        <Source type="geojson" data={allData}>
-        {[dataLayer, lineLayer].map((layer, index) => (
-          <Layer key={index} {...layer} />
-        ))}       
-        </Source>
-        
-        {isclicked &&
-        recommendMarker?.map((marker, index) => (
-          <Marker 
-            key={index}
-            longitude={marker.lng}
-            latitude={marker.lat}
-            onClick={MarkeronClick}
-          >
-            <RecommendPin size={20} />
-          </Marker>
-        ))
-        }      
+            
+            <Source type="geojson" data={allData}>
+            {[dataLayer, lineLayer].map((layer, index) => (
+                <Layer key={index} {...layer} />
+            ))}       
+            </Source>
+            
+            {isclicked &&
+            recommendMarker?.map((marker, index) => (
+                <Marker 
+                key={index}
+                longitude={marker.lng}
+                latitude={marker.lat}
+                onClick={() => MarkeronClick(marker)}
+                >
+                <RecommendPin size={20} />
+                </Marker>
+            ))
+            }
 
-        {marker &&
-          <Marker
-          longitude={marker?.lng}
-          latitude={marker?.lat}
-          anchor="bottom"
-          draggable
-          onDragStart={onMarkerDragStart}
-          onDrag={onMarkerDrag}
-          onDragEnd={onMarkerDragEnd}
-          >
-            <Pin size={20} />
-          </Marker>
+        </Map>
+
+        {isclickedrecommend &&
+            <RecommendSheet {...recommendsheet}/>
         }
-        <div className='paperposition'>
-         <SimpleBox/>
-        </div>
-      </Map>
-
-      {isclickedrecommend &&
-        <RecommendSheet {...recommendInfo}/>
-      }
-
 
     </div>
-  );
+    );
 }
 
 export default App;
